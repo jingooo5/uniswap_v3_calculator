f0e9b93f415e1ec9d33177100733dc32523b812d
jingooo5 <52164793+jingooo5@users.noreply.github.com>
2023-07-25 00:37:43 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import styled, { keyframes } from "styled-components/macro";
+import Navbar from "./components/NavBar";
+
+const AppDiv = styled.div`
+  text-align: center;
+`;
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  width: 100%;
+  justify-content: space-between;
+  position: fixed;
+  top: 0;
+  z-index: 1000;
+`;
+
+const BodyWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  min-height: 100vh;
+  padding: ${({ theme }) => theme.navHeight}px 0px 5rem 0px;
+  align-items: center;
+  flex: 1;
+`;
+
+const AppHeader = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const logoSpin = keyframes`
+from {
+  transform: rotate(0deg);
+}
+to {
+  transform: rotate(360deg);
+}
+`;
+
+const AppLogo = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+  @media (prefers-reduced-motion: no-preference) {
+    animation: ${logoSpin} infinite 20s linear;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppDiv>
+      <HeaderWrapper>
+        <Navbar />
+      </HeaderWrapper>
+      <BodyWrapper>
+        <AppHeader>
+          <AppLogo src={logo} />
+        </AppHeader>
+      </BodyWrapper>
+    </AppDiv>
   );
 }
 
